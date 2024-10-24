e4e343a9a4c23920f9d2f5f9422028b78775d71d
yjj <yoojj.dev@gmail.com>
2024-10-24 22:12:14 +0900

    java

---
+++ 1.java-se/src/lang/ClassLoaderTest.java
@@ -0,0 +1,73 @@
+package lang;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+public class ClassLoaderTest {
+
+	// ClassLoader
+	// : 런타임 중 필요한 클래스를 메모리에 동적으로 로드함
+	// : 자바 프로그램은 여러 클래스 로더가 존재하며 계층 구조를 형성  
+	// : 클래스 로더를 통해 로딩된 클래스들은 언로딩 될 수 없음  
+	
+	// Bootstrap ClassLoader
+	// : 최상위 클래스 로더로 JVM 실행시 가장 먼저 실행
+	// : 네이티브 코드로 구현되어 있음
+	
+	// Platform ClassLoader
+	// System ClassLoader
+	// Application ClassLoader
+	// 사용자 정의 클래스 로더
+	
+	class CustomClassLoader extends ClassLoader {
+		
+		@Override 
+		public Class<?> findClass(String name) throws ClassNotFoundException {
+			byte[] b = loadClassData(name);
+			return defineClass(name, b, 0, b.length);
+		}
+		
+	    private byte[] loadClassData(String name) throws ClassNotFoundException {
+	        try(ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+	             InputStream is = getClass().getClassLoader()
+	                     .getResourceAsStream(name.replace('.', File.separatorChar) + ".class")) {
+
+	            if(is == null) {
+	                throw new ClassNotFoundException("Class not found: " + name);
+	            }
+
+	            int nextValue;
+	            while((nextValue = is.read()) != -1) {
+	                byteStream.write(nextValue);
+	            }
+	            
+	            return byteStream.toByteArray();
+	            
+	        } catch(IOException e) {
+	            e.printStackTrace();
+	        }
+	        
+			return null;
+	    }
+		
+	}
+
+	@Test
+	public void test() throws Exception {
+		
+		final CustomClassLoader loader = new CustomClassLoader();
+		
+		Class<?> c = loader.findClass("claxx.User");
+		Object o = c.getDeclaredConstructor().newInstance();
+		Method m = c.getMethod("toString");
+		
+		System.out.println(m.invoke(o));
+
+	}
+	
+}
--- 1.java-se/src/lang/ClassTest.java
+++ 1.java-se/src/lang/ClassTest.java
@@ -11,21 +11,35 @@ public class ClassTest {
 	// : 클래스 로드시 JVM에 의해 자동 생성되는 클래스 (생성자 없음)
 	// : 실행중인 클래스와 인터페이스의 정보를 담는 클래스   
 
-	@Test
-	public void test() throws NoSuchFieldException, SecurityException {
+	@Test  @Ignore
+	public void test() throws Exception {
 		
-		// 정적으로 클래스 로딩 (컴파일 타임) 
+		// 정적으로 클래스 로딩 (컴파일 타임)
+		// 타입 안정성을 보장하며 ClassNotFoundException 이 발생하지 않음
 		//final Class<?> c = String.class;
 				
-		// 동적으로 클래스 로딩 (런타임) 
-		//final Class<?> c = Class.forName("java.lang.String");
+		// 동적으로 클래스 로딩 (런타임)
+		// 타입 안정성을 보장하지 않으며 ClassNotFoundException 처리필요
+		// class 보다 성능이 조금 저하됨
+		// 런타임에 동적으로 클래스를 생성할 경우 사용
+		final Class<?> c = Class.forName("java.lang.String");
+
+		// 인터페이스인지 여부 반환
+		c.isInterface();
 		
-		// 인스턴스 생성 후 타입 반환
-		final Class<?> c = new String().getClass();
+		// 이넘 클래스인지 여부 반환
+		c.isEnum();
+		
+		// 어노테이션 클래스인지 여부 반환
+		c.isAnnotation();
 		
 		// 클래스의 패키지와 이름을 문자열로 반환
 		c.getName();
 		c.getCanonicalName();
+		c.getTypeName();
+		
+		// 클래스의 패키지 정보 반환
+		c.getPackage();
 		
 		// 클래스의 이름을 문자열로 반환
 		c.getSimpleName();
@@ -33,14 +47,26 @@ public class ClassTest {
 		// 부모 클래스 반환
 		c.getSuperclass();
 		
-		// 구현된 인터페이스를 배열로 반환
+		// 구현한 인터페이스를 배열로 반환
 		c.getInterfaces();
+		
+		// 주어진 생성자를 반환 
+		c.getConstructor(int.class);
+		
+		// 클래스의 공개된 생성자를 배열로 반환
+		c.getConstructors();
 		
 		// 클래스의 모든 생성자를 배열로 반환
 		c.getDeclaredConstructors();
 		
+		// 클래스의 공개된 필드를 배열로 반환
+		c.getFields();
+		
 		// 클래스의 모든 필드를 배열로 반환
 		c.getDeclaredFields();
+		
+		// 클래스의 공개된 메소드를 배열로 반환
+		c.getMethods();
 		
 		// 클래스의 모든 메소드를 배열로 반환
 		c.getDeclaredMethods();
@@ -48,18 +74,27 @@ public class ClassTest {
 		// 어노테이션을 배열로 반환
 		c.getAnnotations();
 		
+		// 공개된 중첩 클래스 목록 반환
+		c.getClasses();
+		
+		// 모든 중첩 클래스 목록 반환
+		c.getDeclaredClasses();
+		
 		// 주어진 이름을 가진 공개 정적 변수를 반환
 		c.getField("");
 
 		// 주어진 이름을 가진 비공개 정적 변수를 반환
 		c.getDeclaredField("");
 		
+		// 제네릭 타입 파라미터 정보 반환
+		c.getTypeParameters();
+		
 	}
 	
-	@Test @Ignore
+	@Test
 	public void example() throws ClassNotFoundException {
 		
-		final Class<?> c = Class.forName("java.lang.String");
+		final Class<?> c = String.class;
 		
 		Method[] methods = c.getMethods();
 		for(Method m : methods) {
